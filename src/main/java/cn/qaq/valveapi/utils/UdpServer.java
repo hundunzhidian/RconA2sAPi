14ee06a0a85e4146e5cb59f456c8b8cf190510f4
QAQ <1414290810@qq.com>
2019-08-29 09:53:51 +0800

    延长了rcon建立tcp的超时时间，加入了对老版本起源引擎的查询支持(cs等)

--- src/main/java/cn/qaq/valveapi/utils/UdpServer.java
+++ src/main/java/cn/qaq/valveapi/utils/UdpServer.java
@@ -67,24 +67,11 @@ public class UdpServer {
         }
         return jsonArray;
     }
-
     /**
-     * @return 返回服务器信息 名称name 地图map 玩家数players 延迟time
-     * @param ip 服务器IP:服务器端口
+     * 新引擎
      * */
-    public static final JSONObject getServers(String ip)
+    public static final void sourceServer(byte[] resBytes,JSONObject src) throws Exception
     {
-
-        JSONObject jsonObject=new JSONObject();
-        jsonObject.put("name","");
-        jsonObject.put("map","");
-        jsonObject.put("players","0/0");
-        jsonObject.put("time","0ms");
-        UdpTools udpTools = null;
-        try {
-            String[] ips=ip.split(":");
-            udpTools=new UdpTools();
-            byte[] resBytes=udpTools.SendData(ips[0],Integer.parseInt(ips[1]),UdpTools.hexStrToBinaryStr(UdpTools.A2S_INFO));
             byte[] tmp=new byte[100];
             int j=6;//初始化从第6个字符开始
             //先处理得到服务器名称
@@ -96,7 +83,7 @@ public class UdpServer {
                     break;
             }
             j++;//跳过等于0的这个字节
-            jsonObject.put("name", UdpTools.byteToString(tmp));
+            src.put("name", UdpTools.byteToString(tmp));
             for(int i=0;j<resBytes.length;i++,j++)
             {
                 tmp[i]=resBytes[j];
@@ -104,7 +91,7 @@ public class UdpServer {
                 if(resBytes[j]==0)
                     break;
             }
-            jsonObject.put("map", UdpTools.byteToString(tmp));
+            src.put("map", UdpTools.byteToString(tmp));
             j++;//跳过 0x00这个字节
             Integer k=0;// 0x00出现的次数
             for(;j<resBytes.length;j++)
@@ -122,7 +109,89 @@ public class UdpServer {
                     break;
                 }*/
             }
-            jsonObject.put("players", resBytes[j+3]+"/"+resBytes[j+4]);
+            src.put("players", resBytes[j+3]+"/"+resBytes[j+4]);
+        if(resBytes[j+8]==(byte) 0x01)  src.put("visibility","private");
+    }
+    /**
+     * 老引擎
+     * */
+    public static final void oldSourceServer(byte[] resBytes,JSONObject src)
+    {
+        /**
+         * Source老引擎：
+         *               Address
+         *               Name
+         *               Map
+         *               Folder
+         *               Game
+         * */
+        byte[] tmp=new byte[100];
+        int j=6;//初始化从第6个字符开始
+        //对于老引擎，这个是address值
+        for(;j<resBytes.length;j++)
+        {
+            tmp[j-6]=resBytes[j];
+            if(resBytes[j]==0)
+                break;
+        }
+        j++;//跳过等于0的这个字节
+        for(int i=0;j<resBytes.length;i++,j++)
+        {
+            tmp[i]=resBytes[j];
+
+            if(resBytes[j]==0)
+                break;
+        }
+        j++;//跳过 0x00这个字节
+        src.put("name", UdpTools.byteToString(tmp));
+        for(int i=0;j<resBytes.length;i++,j++)
+        {
+            tmp[i]=resBytes[j];
+
+            if(resBytes[j]==0)
+                break;
+        }
+        src.put("map", UdpTools.byteToString(tmp));
+        j++;//跳过 0x00这个字节
+        Integer k=0;// 0x00出现的次数
+        for(;j<resBytes.length;j++)
+        {
+            //System.out.print(" "+resBytes[j]+" ");
+            if(resBytes[j]==0)
+            {
+                k++;
+                if(k==2) break;//map后面只有两个string类型的数据
+            }
+            //
+/*                if(resBytes[j-2]==0&&resBytes[j-1]==38&&resBytes[j]==2)
+                {
+                    // 0 38 2
+                    break;
+                }*/
+        }
+        src.put("players", resBytes[j+1]+"/"+resBytes[j+2]);
+        if(resBytes[j+6]==(byte) 0x01)  src.put("visibility","private");
+    }
+    /**
+     * @return 返回服务器信息 名称name 地图map 玩家数players 延迟time
+     * @param ip 服务器IP:服务器端口
+     * */
+    public static final JSONObject getServers(String ip)
+    {
+
+        JSONObject jsonObject=new JSONObject();
+        jsonObject.put("name","");
+        jsonObject.put("map","");
+        jsonObject.put("players","0/0");
+        jsonObject.put("time","0ms");
+        jsonObject.put("visibility","public");
+        UdpTools udpTools = null;
+        try {
+            String[] ips=ip.split(":");
+            udpTools=new UdpTools();
+            byte[] resBytes=udpTools.SendData(ips[0],Integer.parseInt(ips[1]),UdpTools.hexStrToBinaryStr(UdpTools.A2S_INFO));
+            if(resBytes[4]==(byte) 0x6d) oldSourceServer(resBytes,jsonObject);
+                else  sourceServer(resBytes,jsonObject);
             jsonObject.put("time",(int)udpTools.getTime()+"ms");
         } catch (SocketTimeoutException e) {
             jsonObject.put("map", "访问超时");
